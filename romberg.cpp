f4ea32521cd6cdcb9de07cbf5593b35a367bb7a5
sabrina <90106865+wansabrina@users.noreply.github.com>
2022-12-21 20:02:29 +0700

    Add files via upload

---
+++ romberg.cpp
@@ -0,0 +1,91 @@
+/*
+Kelompok 6
+Anggota Kelompok:
+    Najma Ulya Agustina	    5025211239
+    Syarifah Thalita Erfany	5025211175
+    Wan Sabrina Mayzura	    5025211023
+
+Soal:
+Salah satu kelemahan dari metode Trapezoidal adalah kita harus menggunakan jumlah interval yang besar 
+untuk memperoleh akurasi yang diharapkan. Buatlah sebuah program komputer untuk menjelaskan bagaimana 
+metode Integrasi Romberg dapat mengatasi kelemahan tersebut.
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// Fungsi yang akan dicari integrasinya
+double f(double x)
+{
+    return 1/x; // Contoh: f(x) = 1/x
+}
+
+// Fungsi untuk menghitung integral dengan metode trapezoidal
+double trapezoidal(double a, double b, int n)
+{
+    // Menghitung lebar subinterval/trapesium
+    double h = (b - a) / n;
+ 
+    // Menghitung jumlah suku pertama dan terakhir
+    // dengan menggunakan fungsi f
+    double r = f(a) + f(b);
+ 
+    // Menjumlahkan suku tengah dalam rumus di atas
+    for (int i = 1; i < n; i++)
+        r += 2 * f(a + i * h);
+ 
+    // lalu mengalikan h/2 dengan r
+    return (h/2) * r;
+}
+
+// Fungsi untuk menghitung integral dengan integrasi Romberg
+double romberg(double a, double b, int n)
+{
+    // Matriks untuk menyimpan nilai integral yang dihitung dengan metode trapezoidal
+    double r[n+1][n+1];
+
+    // Melakukan inisialisasi array dengan hasil dari metode trapezoidal
+    for (int i = 1; i <= n; i++)
+        r[i][1] = trapezoidal(a, b, pow(2, i-1));
+
+    // Menampilkan hasil integral dengan n = 1
+    cout << "Nilai integral dengan n = " << 1 << ": " << r[1][1] << endl;
+
+    // Menggunakan teknik interpolasi Richardson untuk menghitung nilai integral yang lebih akurat
+    // Dan akan menampilkan masing masing hasil integral tiap n dimulai dari n = 2
+    for (int j = 2; j <= n; j++)
+        for (int i = j; i <= n; i++)
+        {
+            r[i][j] = (pow(4, j-1) * r[i][j-1] - r[i-1][j-1]) / (pow(4, j-1) - 1);
+            cout << "Nilai integral dengan n = " << j << ": " << r[i][j] << endl;
+        }
+
+    return r[n][n]; //Hasil akhir yang akan disimpan dalam r[n][n]
+}
+
+int main()
+{
+    // Batas atas dan batas bawah integral
+    double a = 1;
+    double b = 10;
+
+    // Jumlah subinterval/pias yang digunakan
+    int n = 5;
+
+    // Menampilkan hasil integrasi metode Romberg dan Trapezoidal
+    cout << "Hasil integrasi Romberg: " << romberg(a, b, n) << endl;
+    cout << "Hasil integrasi Trapezoidal: " << trapezoidal(a, b, n) << endl;
+
+    return 0;
+}
+
+/*
+Kesimpulan:
+Untuk mengatasi kelemahan dari metode trapezoidal, pada metode romberg tetap digunakan implementasi 
+metode trapezoidal, namun juga digunakan teknik interpolasi Richardson untuk menambah akurasi dari 
+integralnya. Sehingga hasil akurasi metode romberg dapat dibuktikan dengan melihat tingkat errornya 
+yang lebih kecil daripada metode trapezoidal, jika dibandingkan dengan hasil integrasi dengan menggunakan 
+perhitungan integral secara analitik.
+*/
